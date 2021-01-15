fe1aa2f1ec11d9df25ec1e59ebbc10c7caa06e02
Mikdat Yücel <67651356+mkdtycl96@users.noreply.github.com>
2021-01-15 23:53:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 
-from flask import Flask, render_template, request
-import pickle
-import numpy as np
-
-model = pickle.load(open('iri.pkl', 'rb'))
-
-app = Flask(__name__)
-
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
